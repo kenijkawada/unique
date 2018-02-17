94e1ff474aae7d5ffa330888546d9f733d691398
kenjikawada <k.kawada.8.x@gmail.com>
2018-02-17 22:27:25 +0900

    add Users index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, :only => [:index, :show]
   root 'posts#index'
 
   resources :posts

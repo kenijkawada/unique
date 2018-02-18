ac37343e6bc68cc6bf1b8062aa6dc89510f9ab93
kenjikawada <k.kawada.8.x@gmail.com>
2018-02-18 16:41:09 +0900

    add relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :comments
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
+  resources :users, only: [:index, :show]
+
   root 'posts#index'
 
+  resources :relationships, only: [:create, :destroy]
+
   resources :posts
+
+  resources :comments
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"

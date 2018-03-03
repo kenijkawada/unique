544ab779fa6b425404fca43725b641b808d06549
kenjikawada <k.kawada.8.x@gmail.com>
2018-03-04 00:16:59 +0900

    cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,10 +13,12 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [250,350]
+  version :standard do
+    process :resize_to_fill => [250, 350, :north]
+  end
 
-  version :thumb do
-    process resize_to_fit: [200, 200]
+  version :thumbnail do
+    process :resize_to_fit => [200, 200]
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url

7299109d6441d8f9d22696da742791f10b983f08
kenjikawada <k.kawada.8.x@gmail.com>
2018-03-08 22:49:11 +0900

    post model validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+  validates :image, presence: true
+  validates :content, presence: true
+
   belongs_to :user, optional: true, foreign_key: 'user_id'
   has_many :comments, dependent: :destroy
 end

a6acb1c1a66430eaf21018439c1def87af3b57c4
kenjikawada <k.kawada.8.x@gmail.com>
2018-03-09 21:52:52 +0900

    edit notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  def after_sign_up_path_for(_resource)
-    new_user_session_path
-  end
-
   def after_sign_in_path_for(_resource)
-    users_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(_resource)
